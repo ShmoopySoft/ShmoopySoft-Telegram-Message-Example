207386969bb094a7d2f3047711f0cc871970f505
ShmoopySoft <info@shmoopysoft.co.za>
2020-02-15 09:50:53 +0200

    Add project files.

---
+++ ShmoopySoft Telegram Message Example/Program.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright(c) 2020 ShmoopySoft
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including 
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the 
+ * following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO 
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
+using System.Net;
+using System.IO;
+
+namespace ShmoopySoft_Telegram_Message_Example
+{
+    class Program
+    {
+        // Insert your Telegram BOT Api Token and Chat Id values here.
+        protected const string apiToken = "<<<!!! INSERT YOUR TELEGRAM BOT API TOKEN HERE !!!>>>";
+        protected const string chatId = "<<<!!! INSERT YOUR TELEGRAM CHANNEL CHAT ID HERE !!!>>>";
+
+        public static void Main(string[] args)
+        {
+            // String variables to store the Telegram URL and message text (url encoded).
+            string urlString = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}";
+            string messageText = "Telegram message test from Visual Studio!";
+
+            // Set the Telegram URL.
+            urlString = String.Format(urlString, apiToken, chatId, messageText);
+
+            try
+            {
+                // Create a web request using the formatted Telegram URL.
+                WebRequest request = WebRequest.Create(urlString);
+
+                // Get the response.  
+                WebResponse response = request.GetResponse();
+
+                // Get the status.  
+                string responseStatus = ((HttpWebResponse)response).StatusDescription;
+
+                // Check the status.
+                if (responseStatus == "OK")
+                {
+                    // Get the stream containing content returned by the server.  
+                    // The using block ensures the stream is automatically closed.
+                    using (Stream dataStream = response.GetResponseStream())
+                    {
+                        // Open the stream using a StreamReader for easy access.  
+                        using (StreamReader reader = new StreamReader(dataStream))
+                        {
+                            // Read the content.  
+                            string responseFromServer = reader.ReadToEnd();
+
+                            // Check the content.
+                            if (responseFromServer.Contains("\"ok\":true"))
+                            {
+                                // Display a confirmation.
+                                Console.WriteLine("Message text was successfully sent to Telegram :-)");
+                            }
+                            else
+                            {
+                                // Display an error.
+                                Console.WriteLine("Failed to send the message text to Telegram :-(");
+                            }
+
+                            // Display the content.  
+                            Console.Write(Environment.NewLine);
+                            Console.WriteLine(responseFromServer);
+                        }
+                    }
+                }
+                else
+                {
+                    // Display an error.
+                    Console.WriteLine("Failed to send the message text to Telegram :-(");
+                    Console.Write(Environment.NewLine);
+                    Console.WriteLine("Response status: " + responseStatus);
+                }
+
+                // Close the response.  
+                response.Close();
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                // Display an error.
+                Console.WriteLine("Failed to send the message text to Telegram :-(");
+                Console.Write(Environment.NewLine);
+                Console.WriteLine(ex.ToString());
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ShmoopySoft Telegram Message Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShmoopySoft Telegram Message Example")]
+[assembly: AssemblyDescription("Sends a message to a Telegram Channel using the Telegram BOT Api.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ShmoopySoft (Pty) Ltd")]
+[assembly: AssemblyProduct("ShmoopySoft Telegram Message Example")]
+[assembly: AssemblyCopyright("Copyright © 2020 by ShmoopySoft (Pty) Ltd")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e38ff0ed-3699-475e-b0c6-85f56f8fd1db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
